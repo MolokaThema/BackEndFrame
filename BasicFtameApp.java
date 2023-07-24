5274974daafd7ef66c3c1bb73770c2a0058da1d1
MolokaThema <140435977+MolokaThema@users.noreply.github.com>
2023-07-24 19:25:07 +0200

    Add files via upload

---
+++ BasicFtameApp.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package basicftameapp;
+
+import za.ac.tut.ui.MyFirstFrame;
+
+/**
+ *
+ * @author ellia
+ */
+public class BasicFtameApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        MyFirstFrame frstFrame; // declare
+        
+        frstFrame = new MyFirstFrame(); // instantiate
+        
+    }
+    
+}
